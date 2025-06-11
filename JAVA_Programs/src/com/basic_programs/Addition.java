058f1316cbc350352c793ce313601dabc574c58e
kalag <kalag@ASUS>
2025-06-11 22:10:54 +0530

    Addition

---
+++ JAVA_Programs/src/com/basic_programs/Addition.java
@@ -0,0 +1,20 @@
+package com.basic_programs;
+
+import java.util.Scanner;
+
+public class Addition {
+
+	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+		System.out.println("enter num");
+		int a=sc.nextInt();
+		System.out.println("enter num");
+		int b=sc.nextInt();
+		
+		System.out.println(a+"+"+b+"="+(a+b));
+		
+
+	}
+
+}
+
